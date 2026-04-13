8f002909681518781d0a2f0bf5609f68988eae9c
besnarliz Faria <besnarlizfaria@gmail.com>
2026-04-12 20:03:55 -0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+// NAVBAR SCROLL
+window.addEventListener("scroll", () => {
+  let nav = document.querySelector(".navbar-pro");
+  nav.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+// ANIMACIONES
+let animados = document.querySelectorAll(".animar");
+
+window.addEventListener("scroll", () => {
+  animados.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("activo");
+    }
+  });
+});
+
+//ENCUESTA
+let form = document.getElementById("formulario");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let rol = document.getElementById("rol").value;
+  let interes = document.getElementById("interes").value;
+  let habilidades = document.getElementById("habilidades").value;
+  let ubicacion = document.getElementById("ubicacion").value;
+
+  try {
+    await addDoc(collection(db, "talento_cencosud"), {
+      rol,
+      interes,
+      habilidades,
+      ubicacion,
+      fecha: new Date()
+    });
+
+    document.getElementById("mensaje").innerText = "✅ Tu información fue registrada correctamente";
+    form.reset();
+
+  } catch (error) {
+    document.getElementById("mensaje").innerText = "❌ Error al registrar";
+    console.error(error);
+  }
+});
